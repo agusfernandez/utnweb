e1f0d1f21569d663ac5f603988120dce42919fc5
agusfernandez <agustina.fer91@gmail.com>
2024-07-08 22:35:10 -0300

    Firt Files

---
+++ app/menu.js
@@ -0,0 +1,4 @@
+document.querySelector('.toggle').addEventListener('click', function(){
+    document.querySelector('.menu-content').classList.toggle('show');
+    document.querySelector('.toggle').classList.toggle('active');
+});
